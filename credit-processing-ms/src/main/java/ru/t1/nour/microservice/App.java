0d3623f7fc56b7d5db405829d6c861461618c63a
Nournicat <ploxind@bk.ru>
2025-09-18 12:37:51 +0400

    Revert "feat: init project"
    This reverts commit e45b13d2b8aa33d739c41720041fb4a952be3cbd.

--- credit-processing-ms/src/main/java/ru/t1/nour/microservice/App.java
+++
@@ -1,9 +0,0 @@
-package ru.t1.nour.microservice;
-
-import org.springframework.boot.SpringApplication;
-
-public class App {
-    public static void main(String[] args) {
-        SpringApplication.run(App.class);
-    }
-}
