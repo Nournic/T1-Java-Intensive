956e92d7d810fd0a818cfa847b4fac80d4aed94a
Nournic <ploxind@bk.ru>
2025-09-30 12:36:20 +0400

    feat: forgot invoke service method from KafkaEventListener

--- credit-processing-ms/src/main/java/ru/t1/nour/microservice/service/ProductRegistryService.java
+++ credit-processing-ms/src/main/java/ru/t1/nour/microservice/service/ProductRegistryService.java
@@ -1,4 +1,7 @@
 package ru.t1.nour.microservice.service;
 
+import ru.t1.nour.microservice.model.dto.kafka.ClientProductEventDTO;
+
 public interface ProductRegistryService {
+    void createByEvent(ClientProductEventDTO event);
 }
--- credit-processing-ms/src/main/java/ru/t1/nour/microservice/service/impl/kafka/KafkaEventListener.java
+++ credit-processing-ms/src/main/java/ru/t1/nour/microservice/service/impl/kafka/KafkaEventListener.java
@@ -5,13 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import ru.t1.nour.microservice.model.dto.kafka.ClientProductEventDTO;
+import ru.t1.nour.microservice.service.ProductRegistryService;
 
 @Component
 @Slf4j
 @RequiredArgsConstructor
 public class KafkaEventListener {
+    private final ProductRegistryService productRegistryService;
+
     @KafkaListener(topics = {"client_credit_products"})
     public void handleClientProductCreditEvent(ClientProductEventDTO event){
         log.info("Received ClientProductEvent: {}", event);
+
+        productRegistryService.createByEvent(event);
     }
 }
